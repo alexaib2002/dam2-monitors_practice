fa38d3e8a289a62875d474bda6a8a3df2000b0cd
alexaib <alexaib@outlook.es>
2023-02-06 22:37:24 +0100

    Adjusted time scale

--- src/org/dam/uem/relays/Runner.java
+++ src/org/dam/uem/relays/Runner.java
@@ -1,7 +1,7 @@
 package org.dam.uem.relays;
 
 public class Runner extends Thread {
-    public static long MAX_TIME = 3l;
+    public static long MAX_TIME = 5l;
     private int runnerId;
 
     public Runner(int runnerId) {
@@ -16,7 +16,7 @@ public class Runner extends Thread {
                 wait();
             }
             System.out.printf("%s is running now!\n", runnerId);
-            Thread.sleep((long) (Math.random() * MAX_TIME));
+            Thread.sleep((long) (Math.random() * MAX_TIME * 1000));
             System.out.printf("%s has finished their run\n", runnerId);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
